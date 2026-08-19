dbf72b8b1b2e4dcfb6dbd9ba7cc7ee992175bbd4
Bauman <baumancastignani@hotmail.com>
2026-08-19 12:07:38 -0300

    Add basic triangle draw

--- Engine/src/Renderer.cpp
+++ Engine/src/Renderer.cpp
@@ -9,4 +9,10 @@ Renderer::Renderer(Window* window)
 void Renderer::Render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex2d(-0.5f, -0.5f);
+    glVertex2d(0.0f, 0.5f);
+    glVertex2d(0.5f, -0.5f);
+    glEnd();
 }
